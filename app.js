8e3c7df00eb026cc1d30d278af22e86ad2c9c10e
Abdullah <m10abdullah09@gmail.com>
2025-11-28 14:55:37 +0500

    Update app.js

--- app.js
+++ app.js
@@ -64,7 +64,9 @@ createBtn.onclick = async ()=>{
   await setDoc(doc(db,"rooms",roomId),{password});
   roomIdDisplay.textContent=`Room ID: ${roomId}`;
   roomPassDisplay.textContent=`Password: ${password}`;
-  const link = `${window.location.origin}?room=${roomId}&pass=${password}`;
+const link = `${window.location.origin}/msgapp/?room=${roomId}&pass=${password}`;
+
+  
   linkDisplay.innerHTML=`Share link: <a href="${link}" target="_blank" style="color:#0d6efd;">${link}</a>`;
   enterChat();
 }
@@ -81,8 +83,8 @@ joinRoomBtn.onclick = async ()=>{
   currentRoomId=roomId;
   roomIdDisplay.textContent=`Room ID: ${roomId}`;
   roomPassDisplay.textContent=`Password: ${password}`;
-  linkDisplay.innerHTML=`Share link: <a href="${window.location.origin}?room=${roomId}&pass=${password}" target="_blank" style="color:#0d6efd;">${window.location.origin}?room=${roomId}&pass=${password}</a>`;
-  enterChat();
+ linkDisplay.innerHTML = `Share link: <a href="${window.location.origin}/msgapp/?room=${roomId}&pass=${password}" target="_blank" style="color:#0d6efd;">${window.location.origin}/msgapp/?room=${roomId}&pass=${password}</a>`;
+
 }
 
 // AUTO JOIN (SAFE + FIXED VERSION)
@@ -116,7 +118,8 @@ window.addEventListener("load", async () => {
     roomIdDisplay.textContent = `Room ID: ${urlRoom}`;
     roomPassDisplay.textContent = `Password: ${urlPass}`;
     linkDisplay.innerHTML =
-      `Share link: <a href="${window.location.origin}?room=${urlRoom}&pass=${urlPass}" target="_blank" style="color:#0d6efd;">${window.location.origin}?room=${urlRoom}&pass=${urlPass}</a>`;
+  `Share link: <a href="${window.location.origin}/msgapp/?room=${urlRoom}&pass=${urlPass}" target="_blank" style="color:#0d6efd;">${window.location.origin}/msgapp/?room=${urlRoom}&pass=${urlPass}</a>`;
+
 
     enterChat();
 
@@ -177,5 +180,6 @@ function listenMessages(){
     messagesDiv.scrollTop=messagesDiv.scrollHeight;
   });
 }
+
 
 
